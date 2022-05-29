0775f7e21086ae6214586ee523b2980ac4879ac9
Pathum <pathumhdes@gmail.com>
2022-05-29 12:23:41 +0530

    Refactor the code so that it adheres to the  Single Responsibility Principle

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header class="header">
+    <nav class="header-nav" />
+    <div class="container">
+      <h1>{{ listName }}</h1>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {
+  },
+  props: {
+    listName: String
+  }
+})
+export default class Header extends Vue {
+    listName!: string
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  &-nav {
+    background: teal;
+    width: 100%;
+    height: 56px;
+  }
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,8 @@
 <template>
   <div>
-    <header class="header">
-      <nav class="header-nav" />
-      <div class="container">
-        <h1>My To-Do List</h1>
-      </div>
-    </header>
+    <Header listName="My To-Do List" />
     <main>
-      <div class="container">
-        <div class="todo-list">
-          <div
-            v-for="{ id, title, completed } in todos"
-            :key="id"
-            class="todo-list__task"
-          >
-            <span :class="{ 'todo-list__task--completed': completed }">
-              {{ title }}
-            </span>
-          </div>
-        </div>
-      </div>
+      <ToDoCards :todos="todos" />
     </main>
   </div>
 </template>
@@ -27,8 +10,13 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import { ITodo } from '@/types';
+import { Api } from '@/api'
+import Header from '@/components/Header.vue'
+import ToDoCards from '@/components/ToDoCards.vue'
+
 @Options({
   components: {
+    Header, ToDoCards
   },
   props: {
     
@@ -36,50 +24,15 @@ import { ITodo } from '@/types';
 })
 export default class Home extends Vue {
   todos: ITodo[] = []
-  mounted() {
-    this.fetchTodos()
+  async mounted() {
+    this.todos = await this.fetchTodos();
   }
-  fetchTodos(): void {
-    fetch('https://jsonplaceholder.typicode.com/todos/')
-      .then(response => response.json())
-      .then((todos: ITodo[]) => (this.todos = todos))
+  async fetchTodos(): Promise<ITodo[]> {
+    const api = new Api('todos')
+    return await api.fetch()
   }
 }
 </script>
 
 <style scoped lang="scss">
-.header {
-  width: 100%;
-  &-nav {
-    background: teal;
-    width: 100%;
-    height: 56px;
-  }
-}
-.container {
-  padding: 1.5rem;
-}
-.todo-list {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: stretch;
-  &__task {
-    width: 24%;
-    padding: 1.5rem;
-    margin: 0.5%;
-    text-align: left;
-    color: #4169e1;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
-    &:hover {
-      box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25),
-        0 10px 10px rgba(0, 0, 0, 0.22);
-    }
-    &--completed {
-      color: #2e8b57;
-      text-decoration: line-through;
-    }
-  }
-}
 </style>
